38251d182d1c6af3301e26abe5c083a339795376
Kaeye <kaeyedingemans@hotmail.com>
2021-10-26 16:33:48 +0200

    added code from prototyping assignment 3

--- ambient light/src/main.cpp
+++ ambient light/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+const int ledPin = 11;
+const int sensorPin = A1;
+
+int sensorValue = 0;
+int pinValue = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = map(analogRead(sensorPin), 0, 1023, 0, 255);
+  delay(5);
+  Serial.print("sensor: ");
+  Serial.println(sensorValue);
+
+  pinValue = -sensorValue + 255;
+  analogWrite(ledPin, pinValue);
+  delay(5);
+  Serial.print("pin: ");
+  Serial.println(pinValue);
 }
