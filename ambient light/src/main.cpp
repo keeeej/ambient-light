6041a8d50e3f107566910c0373b67772e22faf07
Kaeye <kaeyedingemans@hotmail.com>
2021-10-26 17:12:28 +0200

    added runtimer library and cleaned code

--- ambient light/src/main.cpp
+++ ambient light/src/main.cpp
@@ -1,27 +1,33 @@
 #include <Arduino.h>
+#include "runtimer.h"
 
 const int ledPin = 11;
 const int sensorPin = A1;
 
+intervalTimer timer1(2000);
+
 int sensorValue = 0;
 int pinValue = 0;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   
   pinMode(ledPin, OUTPUT);
   pinMode(sensorPin, INPUT);
 }
 
-void loop() {
-  sensorValue = map(analogRead(sensorPin), 0, 1023, 0, 255);
-  delay(5);
-  Serial.print("sensor: ");
-  Serial.println(sensorValue);
-
-  pinValue = -sensorValue + 255;
-  analogWrite(ledPin, pinValue);
-  delay(5);
-  Serial.print("pin: ");
-  Serial.println(pinValue);
+void loop() 
+{
+  if (timer1.expired())
+  {
+    sensorValue = map(analogRead(sensorPin), 0, 1023, 0, 255);
+    pinValue = -sensorValue + 255;
+    analogWrite(ledPin, pinValue);
+  
+    Serial.print("sensor: ");
+    Serial.print(sensorValue);
+    Serial.print(" | pin: ");
+    Serial.println(pinValue);
+  }
 }
